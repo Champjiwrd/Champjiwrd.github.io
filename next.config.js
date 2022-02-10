bbbf20fdec31ae28a4988523b93e632fb37562c0
Warodom <warodom@mm.co.th>
2022-02-10 10:46:35 +0700

    edit cdn

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
   assetPrefix: isProd ? '/Champjiwrd.github.io/' : '',
 };
