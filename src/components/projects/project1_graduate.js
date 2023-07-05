f863eb83198dbd37ce1f1a7c05b671575285c491
Warodom Baiudom <warodom@mm.co.th>
2023-07-05 12:50:05 +0700

    Update project1_graduate.js

--- src/components/projects/project1_graduate.js
+++ src/components/projects/project1_graduate.js
@@ -11,10 +11,10 @@ import {
   Heading,
   UnorderedList,
   ListItem,
+  Image,
 } from '@chakra-ui/react';
 // import ProjectItem from "../projectItem";
 import ProjectItem from '../projectItem';
-import { Carousel } from 'react-carousel-minimal';
 import 'react-awesome-slider/dist/styles.css';
 
 export default function Project1() {
@@ -23,12 +23,6 @@ export default function Project1() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const gradeStyle = { color: '#FFBB56', fontSize: 14 };
 
-  const data = [
-    {
-      image: '/projects/cctv.gif',
-      caption: '',
-    },
-  ];
   return (
     <div>
       <div onClick={onOpen}>
@@ -58,24 +52,19 @@ export default function Project1() {
               System used for tracking a suspect person who has exactly what is
               needed (such as shirt, bag, color) from CCTV.
             </Box>
-            <Heading mt={12} mb={3} size="md">
+            <Heading mt={12} size="md">
               Result Demo
             </Heading>
-            <Carousel
-              data={data}
-              time={3000}
-              width="850px"
-              height="500px"
-              radius="10px"
-              style={{
-                textAlign: 'center',
-                maxWidth: '850px',
-                maxHeight: '500px',
-                margin: 'auto',
-              }}
-            />
+
+            <Box mt={5}>
+              <Image
+                borderRadius={10}
+                boxSize={'full'}
+                src="/projects/cctv.gif"
+              />
+            </Box>
             <Heading mt={12} size="md">
-              TECHNOLOGIES
+              TOOLS
             </Heading>
             <UnorderedList mt={3}>
               <ListItem>YOLOv4 Object Detection</ListItem>
